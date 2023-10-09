2ff2e8238628a1b42fb483bbf70f6543e3f49638
aylen luquez <aylen968@gmail.com>
2023-10-08 21:43:56 -0300

    day1

---
+++ index.js
@@ -0,0 +1,22 @@
+let randomNumber1 = Math.floor((Math.random() * 6) + 1);
+let randomNumber2 = Math.floor((Math.random() * 6) + 1);
+
+let randomDiceImage = "dice" + randomNumber1 + ".png";
+
+let randomImageSource1 = "images/dice" + randomNumber1 + ".png" ;
+let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1)
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2)
+
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 wins"
+}
+else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 wins!"
+}
+else {
+    document.querySelector("h1").innerHTML = "Try Again"
+}

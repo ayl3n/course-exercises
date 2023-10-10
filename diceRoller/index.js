eb59f9adf5f85c07d6ee2b3c73be365ea25da9b4
aylen luquez <aylen968@gmail.com>
2023-10-09 21:00:19 -0300

    day2

--- diceRoller/index.js
+++ diceRoller/index.js
@@ -7,8 +7,8 @@ let randomImageSource1 = "images/dice" + randomNumber1 + ".png" ;
 let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
 
-document.querySelector("img1")[0].setAttribute("src", randomImageSource1)
-document.querySelector("img2")[1].setAttribute("src", randomImageSource2)
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1)
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2)
 
 
 if (randomNumber1 > randomNumber2){

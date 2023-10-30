c8bd7d9295a2a89840693c0be3f73a1aefccbb57
aylen luquez <aylen968@gmail.com>
2023-10-29 22:42:49 -0300

    sounds and pressed effect

--- jQuery/Simon Game/index.js
+++ jQuery/Simon Game/index.js
@@ -1,8 +1,16 @@
- jQuery(".btn").click(function(){
+prompt("hi");
+console.log("holi");
+let userClickedPattern = [];
+let gamePattern = [];
+let buttonColours = ["red", "blue", "green", "yellow"]
+ 
+ $(".btn").click(function(){
     
     let userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour); 
 
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
  });
 
 function nextSequence(){
@@ -14,12 +22,21 @@ function nextSequence(){
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100)
 
-    let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
+    playSound(randomChosenColour);
     
-} 
+}
 
-let userClickedPattern = [];
-let gamePattern = [];
-let buttonColours = ["red", "blue", "green", "yellow"]
+function playSound(name){
+    
+    let audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
 
+}
+
+function animatePress(currentColour){
+    userChosenColour = $(this).attr("id");
+    $("#" + currentColour).addClass("pressed")
+    setTimeout(function(){
+        $("#" + currentColour).removeClass("pressed")
+    }, 100);
+    }

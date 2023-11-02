9c7a6f58269cc4efd1b37fe896fa0cd447ce6b96
aylen luquez <aylen968@gmail.com>
2023-11-01 23:08:58 -0300

    fixed and completed

--- jQuery/Simon Game/index.js
+++ jQuery/Simon Game/index.js
@@ -1,20 +1,55 @@
-prompt("hi");
-console.log("holi");
+
 let userClickedPattern = [];
 let gamePattern = [];
-let buttonColours = ["red", "blue", "green", "yellow"]
+let buttonColours = ["red", "blue", "green", "yellow"];
+let started = false;
+let level = 0;
  
- $(".btn").click(function(){
+$(document).keydown(function(){
+    
+    if (!started){
+        $("h1").text("Level "+ level)
+        nextSequence()
+        started = true;
+    }   
+})
+
+
+$(".btn").click(function(){
     
     let userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour); 
 
     playSound(userChosenColour);
     animatePress(userChosenColour);
- });
+    checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel){
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+
+        if (userClickedPattern.length === gamePattern.length){
+
+            setTimeout(function () {
+              nextSequence();
+            }, 1000);
+        } 
+        
+    } else{
+        $("h1").text("Game Over, Press Any Key to Start")
+        let audioWrong = new Audio ("sounds/wrong.mp3");
+        audioWrong.play();
+        setTimeout(function(){
+            $("body").addClass("game-over")});
+        setTimeout(function(){
+            $("body").removeClass("game-over")},200)
+        startOver();
+        };
+}
+
 
 function nextSequence(){
-    
+    userClickedPattern = [];
     level++;
     $("h1").text("Level " + level);
     let randomNumber = Math.floor(Math.random()*4);
@@ -36,7 +71,6 @@ function playSound(name){
 }
 
 function animatePress(currentColour){
-    userChosenColour = $(this).attr("id");
     $("#" + currentColour).addClass("pressed")
     setTimeout(function(){
         $("#" + currentColour).removeClass("pressed")
@@ -44,17 +78,8 @@ function animatePress(currentColour){
 }
 
 
-let started = false;
-let level = 0;
-
-
-$(document).keydown(function(){
-    
-    if (!started){
-        $("h1").text("Level "+ level)
-        nextSequence()
-        started = true;
-    }   
-})
-
-
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
